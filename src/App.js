c4c3939fe26b52cf46f41e8163ec8cb238bbc783
duanzin <lucaduan@gmail.com>
2022-12-17 20:09:30 -0300

    layout do topo e menu das paginas principais criado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import GlobalStyle from "./globalStyle";
-import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Context from './Context';
+import Context from "./Context";
 import Login from "./Login";
 import Cadastro from "./Cadastro";
-
+import Habitos from "./Habitos";
+import Hoje from "./Hoje";
+import Historico from "./Historico";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-        <Routes>
-          <Route path="/" exact={true} element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-        </Routes>
+      <Routes>
+        <Route path="/" exact={true} element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/habitos" element={<Habitos />} />
+        <Route path="/hoje" element={<Hoje />} />
+        <Route path="/historico" element={<Historico />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/Habitos.js
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+export default function Habitos(props) {
+  const percentage = 66;
+
+  return (
+    <>
+      <Header>
+        <p>TrackIt</p>
+        <img
+          src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"
+          alt="pfp"
+        ></img>
+      </Header>
+      <Main></Main>
+      <Footer>
+        <Link to="/habitos">Hábitos</Link>
+        <div>
+          <Link to="/hoje">
+            <CircularProgressbar
+              value={percentage}
+              text="Hoje"
+              styles={buildStyles({
+                pathColor: `#FFFFFF`,
+                textColor: "#FFFFFF",
+                trailColor: "#52b6ff",
+              })}
+            />
+          </Link>
+        </div>
+        <Link to="/historico">Histórico</Link>
+      </Footer>
+    </>
+  );
+}
+
+const Header = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 18px;
+  background: #126ba5;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  p {
+    font-family: "Playball", cursive;
+    font-size: 38.982px;
+    line-height: 49px;
+    color: #ffffff;
+  }
+  img {
+    width: 51px;
+    height: 51px;
+    border-radius: 98.5px;
+  }
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 70px 0;
+  background: #e5e5e5;
+  h2 {
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126ba5;
+  }
+`;
+
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  height: 70px;
+  padding: 0 36px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background: #ffffff;
+
+  a {
+    font-size: 17.976px;
+    line-height: 22px;
+    text-decoration: none;
+    color: #52b6ff;
+  }
+  div {
+    width: 91px;
+    height: 91px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #52b6ff;
+    margin-bottom: 40px;
+    padding: 6px;
+    border-radius: 50px;
+  }
+`;
